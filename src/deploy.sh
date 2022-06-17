4b806fb24574357518555e4f95e2b9e21859c3fc
lieven <lieven.merckx@gmail.com>
2022-06-17 17:21:07 +0200

    autorestart at boot

--- src/deploy.sh
+++ src/deploy.sh
@@ -2,8 +2,10 @@ sudo cp udp2redis.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl restart udp2redis
 sudo systemctl status udp2redis
+sudo systemctl enable udp2redis
 
 sudo cp ws2redis.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl restart ws2redis
 sudo systemctl status ws2redis
+sudo systemctl enable ws2redis
